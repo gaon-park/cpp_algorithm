65d1c0d31650eba76a0e6df54e8a4a753e750054
GAON PARK <ondol1224@gmail.com>
2023-10-25 16:49:55 +0900

    피보나치 수 2

---
+++ 0x10 다이나믹 프로그래밍/2748.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+long long d[95];
+int n;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> n;
+	d[1] = 1, d[2] = 1;
+	for (int i = 3; i <= n; i++) d[i] = d[i - 1] + d[i - 2];
+	cout << d[n];
+}
