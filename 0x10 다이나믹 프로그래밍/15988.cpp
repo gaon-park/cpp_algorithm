7567cf5217d22f9edce14a8343579897dc300c83
GAON PARK <ondol1224@gmail.com>
2023-10-25 16:47:16 +0900

    1, 2, 3 더하기 3

---
+++ 0x10 다이나믹 프로그래밍/15988.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+long long dp[1'000'005]; 
+int n;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	dp[1] = 1, dp[2] = 2, dp[3] = 4;
+	for (int i = 4; i <= 1'000'000; i++) {
+		dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1'000'000'009;
+	}
+	
+	int T;
+	cin >> T;
+	while (T--) {
+		int n;
+		cin >> n;
+		cout << dp[n] << "\n";
+	}
+}
